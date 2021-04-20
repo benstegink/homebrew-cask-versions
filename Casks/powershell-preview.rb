08317a93e645cbdf4da4efe1696a356430256687
Ben Stegink <benstegink@users.noreply.github.com>
2021-04-20 11:37:50 -0400

    version bump

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -1,5 +1,5 @@
 cask "powershell-preview" do
-  version "7.2.0-preview.3"
+  version "7.2.0-preview.4"
   sha256 "90003e3f76056a0b40ecc978a123db29d48726509dce1d60707ef2f9d8c0a793"
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
